6846ef61357c2ed5c2a0374080363c910b439375
André Vega <ventaslederer@hotmail.com>
2021-12-07 13:31:46 -0600

    Page is almost working correctly regarding the work day schedule. You can now save your reminders and events on local storage. This feature is not optimized yet.

--- src/scripts.js
+++ src/scripts.js
@@ -1,5 +1,6 @@
 
 var currentHourNumberM
+var eventsArray = []
 
 var currentMoment = moment()
 var currentDate = moment().format('dddd, MMMM Do YYYY')
@@ -24,6 +25,7 @@ var hours = [
 var headingDateEl = $("#currentDay")
 var inputsEl = $("<textarea>")
 var buttonsEl = $(".buttons")
+var eventDivEl = $(".row")
 
 headingDateEl.text(currentDate)
 
@@ -31,39 +33,67 @@ var currentHour = currentMoment.format("hh")
 var currentHourMeridium = currentMoment.format("a")
 var currentHourNumber = parseInt(currentHour)
 
-// Make sure that on 12 and 24 hrs you don't have issues!
-// console.log(currentMoment.add(7, "hours").format("hh"))
-
-
+// Fucntions ran on start
 function currentHourFormatConverter() {
     if (currentHourMeridium === "pm" && currentHourNumber !== 12 || currentHourMeridium === "am" && currentHourNumber === 12) {
-        currentHourNumberM = currentHourNumber + 12
+        currentHourNumber = currentHourNumber + 12
     }
 }
-
 function checkHours() {
     for (i= 0; i < hours.length; i++) {
         var hourEl = hours[i]
         var hour = (parseInt(hourEl.attr('id')) )
         hourEl.removeClass("past present future")
-        if (hour < currentHourNumberM ) hourEl.next().addClass("past")
-        if (hour === currentHourNumberM) hourEl.next().addClass("present")
-        if (hour > currentHourNumberM) hourEl.next().addClass("future")
+        if (hour < currentHourNumber ) hourEl.next().addClass("past")
+        if (hour === currentHourNumber) hourEl.next().addClass("present")
+        if (hour > currentHourNumber) hourEl.next().addClass("future")
     }
 }
+console.log(eventsArray)
+function retrieveSavedEventsFromLocalStorage() {
+    var localStorageArray = JSON.parse(localStorage.getItem("Events"))
+    if (localStorageArray !== null) eventsArray = localStorageArray
+    console.log(eventsArray)
+    
+    
+}
+function fillEmptyEventsWithPlaceholders() {
+    for(i= 9; i < 21; i++) {
+        var dateTextareaPlaceholderEl = $(`#${i}`).next()
+        dateTextareaPlaceholderEl.text("Schedule a reminder or an event here.")
+    }
+}
+function renderSavedEvents() {
+    savedEventsLocalStorArray = JSON.parse(localStorage.getItem("Events"))
+    if (savedEventsLocalStorArray !== null) {
+        for (i= 0; i < savedEventsLocalStorArray.length; i++) {
+            var dateTextareaEl = $(`#${savedEventsLocalStorArray[i].time}`).next()
+            dateTextareaEl.text(savedEventsLocalStorArray[i].event) 
+        }
+    }
+    
+}
 
-buttonsEl.on('click', function() {
-    console.log($(this).prev().val())
+// Functions from event listeners
+function saveOnLocalStorage() {
+    localStorage.setItem("Events", JSON.stringify(eventsArray))
+}
+eventDivEl.on('click', '.saveBtn', function () {
+    var inputText = $(this).prev().val()
+    var hour = $(this).parent().children(0).attr('id')
+    var eventObject = {
+        time: hour,
+        event: inputText
+    }
+    eventsArray.push(eventObject)
+    console.log(eventObject)
+    console.log(inputText)
+    console.log(hour)
+    saveOnLocalStorage()
 })
 currentHourFormatConverter();
 checkHours()
+retrieveSavedEventsFromLocalStorage()
+fillEmptyEventsWithPlaceholders()
+renderSavedEvents()
 
-
-
-// var mainHeadingEl = $('#main-heading');
-// var jumbotronEl = $('.jumbotron')
-// var testEl = $('<h1>')
-// mainHeadingEl.text('Hello')
-// testEl.text = 'hello';
-// // mainHeadingEl.attr('class', 'red')
-// mainHeadingEl.addClass('red');
